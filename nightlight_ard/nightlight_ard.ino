3199207275008477ae600de7d0e0403b06ed3614
yair reshef <yair99@gmail.com>
2015-12-21 18:22:09 +0200

    added running avrage class

--- nightlight_ard/nightlight_ard.ino
+++ nightlight_ard/nightlight_ard.ino
@@ -1,39 +1,27 @@
-/* Welcome to DmxSimple. This library allows you to control DMX stage and
-** architectural lighting and visual effects easily from Arduino. DmxSimple
-** is compatible with the Tinker.it! DMX shield and all known DIY Arduino
-** DMX control circuits.
-**
-** DmxSimple is available from: http://code.google.com/p/tinkerit/
-** Help and support: http://groups.google.com/group/dmxsimple       */
-
-/* To use DmxSimple, you will need the following line. Arduino will
-** auto-insert it if you select Sketch > Import Library > DmxSimple. */
-a
 #include <DmxSimple.h>
+#include "RunningAverage.h"
 
 void setup() {
-  /* The most common pin for DMX output is pin 3, which DmxSimple
-  ** uses by default. If you need to change that, do it here. */
+  Serial.begin(57600);
   DmxSimple.usePin(3);
+  DmxSimple.maxChannel(12);
 
-  /* DMX devices typically need to receive a complete set of channels
-  ** even if you only need to adjust the first channel. You can
-  ** easily change the number of channels sent here. If you don't
-  ** do this, DmxSimple will set the maximum channel number to the
-  ** highest channel you DmxSimple.write() to. */
-  DmxSimple.maxChannel(4);
+  pinMode(A9, INPUT);
+  pinMode(A8, INPUT);
+  pinMode(A7, INPUT);
+
 }
 
 void loop() {
-  int brightness;
-  /* Simple loop to ramp up brightness */
-  for (brightness = 0; brightness <= 255; brightness++) {
-    
-    /* Update DMX channel 1 to new brightness */
-    DmxSimple.write(1, brightness);
-    
-    /* Small delay to slow down the ramping */
-    delay(10);
-  }
+  Serial.println(analogRead(A9));
+  Serial.println(analogRead(A8));
+  Serial.println(analogRead(A7));
+
+  delay(10);
+
+  //DmxSimple.write(1, brightness);
+
 
 }
+
+
