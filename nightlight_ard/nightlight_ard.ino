fc5a7b60cd6b81a544e68ac6edb2c15f35280043
yair reshef <yair99@gmail.com>
2015-12-22 00:36:50 +0200

    first bar on oled working

--- nightlight_ard/nightlight_ard.ino
+++ nightlight_ard/nightlight_ard.ino
@@ -1,6 +1,21 @@
+//NIGHTLIGHT
+// https://github.com/5shekel/lightnight
+
 #include <DmxSimple.h>
 
-#define smooth 1
+#include <Wire.h>  // Include Wire if you're using I2C
+#include <SPI.h>  // Include SPI if you're using SPI
+#include <SFE_MicroOLED.h>  // Include the SFE_MicroOLED library
+// https://learn.sparkfun.com/tutorials/micro-oled-breakout-hookup-guide
+//////////////////////////
+// MicroOLED Definition //
+//////////////////////////
+#define PIN_RESET 9  // Connect RST to pin 9 (req. for SPI and I2C)
+#define PIN_DC    8  // Connect DC to pin 8 (required for SPI)
+#define PIN_CS    10 // Connect CS to pin 10 (required for SPI)
+#define DC_JUMPER 0
+// Also connect pin 13 to SCK and pin 11 to MOSI
+MicroOLED oled(PIN_RESET, PIN_DC, PIN_CS);
 
 #include "SmoothAnalogInput.h"
 
@@ -8,10 +23,14 @@ SmoothAnalogInput smp9;
 SmoothAnalogInput smp8;
 SmoothAnalogInput smp7;
 
-int samples;
+int samples, i;
 
 void setup() {
   Serial.begin(57600);
+
+  oled.begin();
+  oled.clear(ALL);  // Clear the display's memory (gets rid of artifacts)
+  oled.display();   
 
   pinMode(A9, INPUT);
   pinMode(A8, INPUT);
@@ -28,28 +47,31 @@ void setup() {
 }
 
 void loop() {
-
+int width = 24;
+int height = 64;
   int sm7 = smp7.read();
   int sm8 = smp8.read();
   int sm9 = smp9.read();
-
-  //if (smooth) {
+  //sm8 = map(sm8, 0, 50, 0, 64);
+  //oled.rectFill(0,0,64,i++);
+ // if(i>63) i=0;
+ oled.rectFill(0, 0, width, constrain(sm8, 0, 50));
+ 
+  oled.display(); oled.clear(PAGE);
+/*
   Serial.print(sm7);
   Serial.print(" ");
   Serial.print(sm8);
   Serial.print(" ");
   Serial.print(sm9);
-  //Serial.println();
-    Serial.print(" ");
-
-  //}  else {
+  Serial.print(" ");
   Serial.print(analogRead(A7));
   Serial.print(" ");
   Serial.print(analogRead(A8));
   Serial.print(" ");
   Serial.print(analogRead(A9));
   Serial.println();
-  //}
+*/
   delay(10);
   //DmxSimple.write(1, brightness);
 }
